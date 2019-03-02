170fcdc74a7b11c1708803176f0fddf3f582c3c1
KyleWilliamz <kjw22@buffalo.edu>
2019-03-02 00:36:23 -0500

    edit feature option added

--- electron-app/addressbook.js
+++ electron-app/addressbook.js
@@ -14,8 +14,13 @@ $('#addbtn').on('click', () => {
 
   let name = $('#contactname').val()
   let number = $('#contactnumber').val()
+  let mobile = $('#contactnumberm').val()
+  let address = $('#contactAddress').val()
+  let company = $('#contactCompany').val()
+  let birthday = $('#contactBirthday').val()
+  let email = $('#contactEmail').val()
 
-  fs.appendFileSync('contacts.txt', name+","+number+'\n', (err) => {
+  fs.appendFileSync('contacts.txt', name+","+number+","+ mobile+","+ address+","+ company+","+birthday+","+ email+'\n', (err) => {
     if (err) throw err;
     console.log("the data was appended!");
   });
@@ -24,10 +29,19 @@ $('#addbtn').on('click', () => {
 
 })
 
-function addEntry(name, number){
+function validate(name, number, mobile, address, company, birthday, email){
+
+}
+
+function addEntry(name, number, mobile, address, company, birthday, email){
   var contact = {};
   contact['name'] = name;
   contact['number'] = number;
+  contact['mobile'] = mobile;
+  contact['address'] = address;
+  contact['company'] = company;
+  contact['birthday'] = birthday;
+  contact['email'] = email;
   contacts.push(contact);
   var index = contacts.length-1;
 
@@ -38,16 +52,50 @@ function addEntry(name, number){
 
 function loadDetails(index){
     var contact = contacts[index];
-    $('#selectedname').text(contact.name);
-    $('#selectednumber').text(contact.number);
+    $('#savebtn').hide();
+    $('#selectedname').val(contact.name);
+    $('#selectedname').prop('readonly', true);
+    $('#selectednumber').val(contact.number);
+    $('#selectednumber').prop('readonly', true);
+    $('#selectedmobile').val(contact.mobile);
+    $('#selectedmobile').prop('readonly', true);
+    $('#selectedadress').val(contact.address);
+    $('#selectedaddress').prop('readonly', true);
+    $('#selectedcompany').val(contact.company);
+    $('#selectedcompany').prop('readonly', true);
+    $('#selectedbirthday').val(contact.birthday);
+    $('#selectedbirthday').prop('readonly', true);
+    $('#selectedemail').val(contact.number);
+    $('#selectedemail').prop('readonly', true);
     $('#deletebtn').off('click');
     $('#deletebtn').on('click', () => {
       deleteEntry(index);
     })
     $('#editbtn').off('click');
     $('#editbtn').on('click', () => {
-      showEditContactModal();
+      $('#selectedname').prop('readonly', false);
+      $('#selectednumber').prop('readonly', false);
+      $('#savebtn').show();
     })
+    $('#savebtn').off('click');
+    $('#savebtn').on('click', () => {
+      editEntry(index);
+    })
+}
+
+function editEntry(index){
+  contacts[index].name = $('#selectedname').val();
+  contacts[index].number = $('#selectednumber').val();
+  fs.truncateSync('contacts.txt');
+  contacts.forEach((contact, index) => {
+  
+    fs.appendFileSync('contacts.txt', contact.name+","+contact.number+","+ mobile+","+ address+","+ company+","+birthday+","+ email+'\n', (err) => {
+      if (err) throw err;
+      console.log("the data was appended!");
+    });
+  })
+  contacts=[];
+  loadAndDisplayContacts();
 }
 
 function deleteEntry(index){
@@ -57,7 +105,7 @@ function deleteEntry(index){
     
     contacts.forEach((contact, index) => {
 
-      fs.appendFileSync('contacts.txt', contact.name+","+contact.number+'\n', (err) => {
+      fs.appendFileSync('contacts.txt', contact.name+","+contact.number+","+ mobile+","+ address+","+ company+","+birthday+","+ email+'\n', (err) => {
         if (err) throw err;
         console.log("the data was appended!");
       });
@@ -90,7 +138,8 @@ function showAddContactModal(){
   ipcRenderer.send('asynchronous-message', 'showAddModal')
 }
 
-function showEditContactModal(){
+function showEditContactModal(index){
+  loadEditDetails(index);
   ipcRenderer.send('asynchronous-message', 'showEditModal')
 }
 
